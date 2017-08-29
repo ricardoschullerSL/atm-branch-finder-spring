e3308874b03d24236bdc29a7733702d4b2c53535
ricardoschullerSL <rschuller@scottlogic.com>
2017-08-29 14:11:00 +0100

    Open data endpoints functional again.

--- src/main/java/ATMBranchFinderSpring/controllers/ATMController.java
+++ src/main/java/ATMBranchFinderSpring/controllers/ATMController.java
@@ -7,12 +7,16 @@ import ATMBranchFinderSpring.repos.BankRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 @RestController
 public class ATMController {
 
     private final ATMRepo atmRepo;
+    private final double LATITUDE_TO_KM = 69;
+    private final double LONGITUDE_TO_KM = 111;
+
 
     @Autowired
     ATMController( ATMRepo atmRepo) {
@@ -30,4 +34,20 @@ public class ATMController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/atms/city/{cityName}", produces="application/json")
     public Collection<ATM> FindAtmsByCity(@PathVariable String cityName) {return atmRepo.FindByCity(cityName); }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/atms/userlocation/{userLatitude}/{userLongitude}/{maxDistance}")
+    public Collection<ATM> FindAtmsByUserLocation(@PathVariable double userLatitude, @PathVariable double userLongitude, @PathVariable double maxDistance) {
+        ArrayList<ATM> atms = (ArrayList<ATM>) atmRepo.GetAll();
+        ArrayList<ATM> filteredAtms = new ArrayList<ATM>();
+
+        for(ATM atm: atms) {
+            double distanceSquared = Math.pow((atm.getGeographicLocation().getLatitude() - userLatitude)*LATITUDE_TO_KM, 2) +
+                    Math.pow((atm.getGeographicLocation().getLongitude() - userLongitude)*LONGITUDE_TO_KM, 2);
+            if (distanceSquared <= maxDistance) {
+                atm.setDistanceSquared(distanceSquared);
+                filteredAtms.add(atm);
+            }
+        }
+        return filteredAtms;
+    }
 }
